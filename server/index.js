bc89d7ae35c991d5a75a876a1b8848c015cb8e5d
marco_dca <marcodecara@gmail.com>
2019-09-20 23:37:52 +0200

    client added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { createServer } from "http";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import server from "./schema";
+import path from "path";
+import cors from "cors";
 
 const port = process.env.PORT || 4000;
 
+//db
 const url =
   process.env.NODE_ENV === "production"
     ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-nvtf4.mongodb.net/test?retryWrites=true`
@@ -17,10 +20,22 @@ mongoose.connection.once("open", () => {
 
 const app = express();
 
-server.applyMiddleware({ app, path: "/graphql" });
+//cross origin requests
+app.use(cors());
 
+//graphql
+server.applyMiddleware({ app, path: "/graphql" });
 const httpServer = createServer(app);
 server.installSubscriptionHandlers(httpServer);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 httpServer.listen(port, () => {
   console.log(`Apollo server on port ${port}`);
