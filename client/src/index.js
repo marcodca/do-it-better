e9d6d06200353c2b30bff5eb2e67acce429a5a1f
marco_dca <marcodecara@gmail.com>
2019-09-21 08:34:14 +0200

    attempt conection graphql client

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient } from 'apollo-client';
+import { getMainDefinition } from 'apollo-utilities';
+import { ApolloLink, split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import * as serviceWorker from './serviceWorker';
 
 //Set the corresponding uri for production and development
 const uri =
   process.env.NODE_ENV === "production"
     ? "/graphql"
-    : "http://localhost:4000/graphql";
+    : "//localhost:4000/graphql";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+    const httpLink = new HttpLink({
+      uri: `http:${uri}`,
+    });
+    
+    const wsLink = new WebSocketLink({
+      uri: `ws:${uri}`,
+      options: {
+        reconnect: true,
+      },
+    });
+    
+    const terminatingLink = split(
+      ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return (
+          kind === 'OperationDefinition' && operation === 'subscription'
+        );
+      },
+      wsLink,
+      httpLink,
+    );
+    
+    const link = ApolloLink.from([terminatingLink]);
+    
+    const cache = new InMemoryCache();
+    
+    const client = new ApolloClient({
+      link,
+      cache,
+    });
+    
+    ReactDOM.render(
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>,
+      document.getElementById('root'),
+    );
 
 
 serviceWorker.unregister();
